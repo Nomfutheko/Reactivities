a42fddffac343a1e59f951b6b13e17a909fcd967
Nomfutheko <thekothandiwe@gmail.com>
2022-09-22 10:56:25 +0200

    End of section 6

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app/App';
+import App from './app/layout/App';
 import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 
